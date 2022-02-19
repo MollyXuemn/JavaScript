8de52f903d1076e9f8b6e50d80011c1c09aaf449
MollyXuemn <46561070+MollyXuemn@users.noreply.github.com>
2022-02-19 17:20:31 +0100

    Push to GitHub JavaScript_1

---
+++ Variable.js
@@ -0,0 +1,33 @@
+
+   /*  
+   1. 使用 window.alert() 弹出警告框。
+    使用 document.write() 方法将内容写到 HTML 文档中。
+    使用 innerHTML 写入到 HTML 元素。
+    使用 console.log() 写入到浏览器的控制台。
+
+
+   2. 以下是 JavaScript 中最​​重要的保留字（按字母顺序）：
+    abstract 	else 	instanceof 	super
+    boolean 	enum 	int 	switch
+    break 	export 	interface 	synchronized
+    byte 	extends 	let 	this
+    case 	false 	long 	throw
+    catch 	final 	native 	throws
+    char 	finally 	new 	transient
+    class 	float 	null 	true
+    const 	for 	package 	try
+    continue 	function 	private 	typeof
+    debugger 	goto 	protected 	var
+    default 	if 	public 	void
+    delete 	implements 	return 	volatile
+    do 	import 	short 	while
+    double 	in 	static 	with
+
+    2.1 在文本字符串中使用反斜杠对代码行进行换行。下面的例子会正确地显示：
+
+    document.write("你好 \
+    世界!");
+
+ */
+
+    
